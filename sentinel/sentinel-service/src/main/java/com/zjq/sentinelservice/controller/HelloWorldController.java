ef95aa2e967d0666d4a3f90967c76ecbed41f941
zhanjq <1187625098@qq.com>
2021-11-23 18:05:18 +0800

    Sentinel案例

--- sentinel/sentinel-service/src/main/java/com/zjq/sentinelservice/controller/HelloWorldController.java
+++ sentinel/sentinel-service/src/main/java/com/zjq/sentinelservice/controller/HelloWorldController.java
@@ -1,7 +1,5 @@
 package com.zjq.sentinelservice.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,12 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "hello")
 public class HelloWorldController {
 
-    private Logger log = LoggerFactory.getLogger(HelloWorldController.class);
-
-
     @GetMapping(value = "/testLimit")
     public String hello(@RequestParam(value = "username",required = false,defaultValue = "詹先生") String username){
-
         return "hello "+username+", I'm sentinel service,follow me,go go go.";
     }
 
